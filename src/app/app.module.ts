0d11cec2603d64da5f4ffbc8d2527eca77253701
klmjr <klmjr@udel.edu>
2021-03-31 11:45:35 -0400

    calendar shows. The command to run on local host is ng serve and i left comments about what the imports do. Will continue working in a few hours

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+//Imports for calendar module and date module
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
+    //Imports the calemdar module 
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
