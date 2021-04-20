648425934e1e7873c34cc3eeaf534b19d4f37a76
wjm2038 <10401572+wjm@users.noreply.github.com>
2021-04-20 12:16:56 -0400

    better UI, need more working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,25 @@ import { AppComponent } from './app.component';
 //Imports for calendar module and date module
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-
+import { CommonModule } from '@angular/common';
+import { NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NgbModalModule,
     BrowserModule,
-    
+    NgbModalModule,
+    BrowserAnimationsModule,
+    CommonModule,
     //Imports the calemdar module 
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory,
-    })
+    }),
+    NgbModule
 
   ],
   providers: [],
